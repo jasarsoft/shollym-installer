367502999bcf45771686fb352c6a2cde1300a18f
Edin Jašarević <edinjasar14@gmail.com>
2017-08-11 16:55:16 +0200

    compress sound

--- compress/compress_sound.bat
+++ compress/compress_sound.bat
@@ -1,3 +1,13 @@
+REM cls
+REM arc a -ep1 -r -ed -mt1 -p144320472947294 -hp -msrep+delta+lzma:a1:mfbt4:d256m:fb273:mc1000000000:lc8 "sound.pack" "sound\*"
+REM pause
+
 cls
-arc a -ep1 -r -ed -mt1 -p144320472947294 -hp -msrep+delta+lzma:a1:mfbt4:d256m:fb273:mc1000000000:lc8 "sound.pack" "sound\*"
+arc a -ep1 -r -ed -mt1 -p144320472947294 -hp -msrep+delta+lzma:a1:mfbt4:d256m:fb273:mc1000000000:lc8 "sound_main.pack" "sound\0_sound.afs"
+pause
+cls
+arc a -ep1 -r -ed -mt1 -p144320472947294 -hp -msrep+delta+lzma:a1:mfbt4:d256m:fb273:mc1000000000:lc8 "sound_club.pack" "sound\e_sound.afs"
+pause
+cls
+arc a -ep1 -r -ed -mt1 -p144320472947294 -hp -msrep+delta+lzma:a1:mfbt4:d256m:fb273:mc1000000000:lc8 "sound_nt-cl.pack" "sound\x_sound.afs"
 pause
