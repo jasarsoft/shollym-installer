3c75ccf85c1b8c2829f5afdd2e1156d06e118ea8
Edin Jašarević <edinjasar14@gmail.com>
2017-08-11 17:53:09 +0200

    compress uni

---
+++ compress/compress_uni.bat
@@ -0,0 +1,9 @@
+cls
+arc a -ep1 -r -ed -mt1 -p144320472947294 -hp -mprecomp+srep+delta+lzma:a1:mfbt4:d256m:fb273:mc1000000000:lc8 "uni_club.pack" "uni\club\*"
+pause
+cls
+arc a -ep1 -r -ed -mt1 -p144320472947294 -hp -mprecomp+srep+delta+lzma:a1:mfbt4:d256m:fb273:mc1000000000:lc8 "uni_national.pack" "uni\national\*"
+pause
+cls
+arc a -ep1 -r -ed -mt1 -p144320472947294 -hp -mprecomp+srep+delta+lzma:a1:mfbt4:d256m:fb273:mc1000000000:lc8 "uni_classic.pack" "uni\classic\*"
+pause
